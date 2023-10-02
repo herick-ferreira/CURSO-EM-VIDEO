03f894845fd926493a850b49fff011ece2437b70
Herick <77906684+herick-ferreira@users.noreply.github.com>
2023-10-02 11:57:54 -0300

    Add files via upload

---
+++ EXERCÍCIOS JAVA E JAVA POO/RepeticaoFor/src/repeticaofor/RepeticaoFor.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repeticaofor;
+
+/**
+ *
+ * @author HericK
+ */
+public class RepeticaoFor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("1\n10");
+        for (int cc= 20;cc <=100;cc+=10){
+            System.out.println(cc);
+        }
+    }
+    
+}
